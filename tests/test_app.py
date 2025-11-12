1d051ba6ab190a3ffad18eea722ba85eaffe40e7
apple <apple@Anmols-MacBook-Pro.local>
2025-11-12 18:06:43 +0530

    flask with jenkins pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Hello, CI/CD World!" in response.data
