977c9f133180b948a12a881c88552d44cd11c0ef
apple <apple@Anmols-MacBook-Pro.local>
2025-11-13 18:42:27 +0530

    pushed new changes to test webhook

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,4 +6,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, CI/CD World!" in response.data
+    assert b"Hello, CI/CD World! and test jenkins with webhook (hope no error comes)" in response.data
